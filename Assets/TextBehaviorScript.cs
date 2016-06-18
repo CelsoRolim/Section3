b5730119f782e07330b566d80280041fa4d6dadc
Celso <cgr@email.com>
2016-06-17 22:23:00 -0400

    Issue: First commit for Project Section 3 (Text101) Dev: celso.guido

---
+++ Assets/TextBehaviorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextBehaviorScript : MonoBehaviour {
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Press any key to start.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			text.text = "Key return pressed.";
+		}
+	}
+}
